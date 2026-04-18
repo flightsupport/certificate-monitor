625119739daf3be5690f3189f82d28c54baac6ca
Thomas Sommer <github@flightsupport.ch>
2026-04-18 08:48:36 +0200

    Fixes by rector

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\MonitorController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn() => redirect(route('login')));
+Route::get('/', fn(): \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector => redirect(route('login')));
 
 Route::middleware(['auth', 'verified'])->group(function (): void {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
